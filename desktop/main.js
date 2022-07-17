e59b4fc002fea3d802189783b70fa979179682ad
rougepied <arnaud@rougepied.com>
2022-07-17 18:49:44 +0200

    update electron
    TODO manage menu

--- desktop/main.js
+++ desktop/main.js
@@ -1,81 +1,101 @@
-'use strict'
+//@ts-check
+const { app, BrowserWindow, Menu } = require("electron");
+const path = require("path");
 
-/* global createWindow */
+let isShown = true;
 
-const { app, BrowserWindow, Menu } = require('electron')
-const path = require('path')
+let win = null;
 
-let isShown = true
-
-app.win = null
-
-app.on('ready', () => {
-  app.win = new BrowserWindow({
+const createWindow = () => {
+  win = new BrowserWindow({
     width: 780,
     height: 462,
     minWidth: 380,
     minHeight: 360,
-    backgroundColor: '#000',
-    icon: path.join(__dirname, { darwin: 'icon.icns', linux: 'icon.png', win32: 'icon.ico' }[process.platform] || 'icon.ico'),
+    backgroundColor: "#000",
+    icon: path.join(
+      __dirname,
+      { darwin: "icon.icns", linux: "icon.png", win32: "icon.ico" }[
+        process.platform
+      ] || "icon.ico"
+    ),
     resizable: true,
-    frame: process.platform !== 'darwin',
-    skipTaskbar: process.platform === 'darwin',
-    autoHideMenuBar: process.platform === 'darwin',
-    webPreferences: { zoomFactor: 1.0, nodeIntegration: true, backgroundThrottling: false }
-  })
+    frame: process.platform !== "darwin",
+    skipTaskbar: process.platform === "darwin",
+    autoHideMenuBar: process.platform === "darwin",
+    webPreferences: {
+      zoomFactor: 1.0,
+      nodeIntegration: true,
+      contextIsolation: false,
+      backgroundThrottling: false,
+    },
+  });
+  
+  win.loadURL(`file://${__dirname}/sources/index.html`);
 
-  app.win.loadURL(`file://${__dirname}/sources/index.html`)
+  win.on("closed", () => {
+    app.quit();
+  });
+  
+  win.on("hide", function () {
+    isShown = false;
+  });
+  
+  win.on("show", function () {
+    isShown = true;
+  });
+};
+
+app.whenReady().then(() => {
   // app.inspect()
 
-  app.win.on('closed', () => {
-    app.quit()
-  })
 
-  app.win.on('hide', function () {
-    isShown = false
-  })
+  app.on("window-all-closed", () => {
+    app.quit();
+  });
 
-  app.win.on('show', function () {
-    isShown = true
-  })
-
-  app.on('window-all-closed', () => {
-    app.quit()
-  })
-
-  app.on('activate', () => {
-    if (app.win === null) {
-      createWindow()
+  app.on("activate", () => {
+    if (win === null) {
+      createWindow();
     } else {
-      app.win.show()
+      win.show();
     }
-  })
-})
-
+  });
+});
+/*
 app.inspect = function () {
-  app.win.toggleDevTools()
-}
+  win.toggleDevTools();
+};
 
 app.toggleFullscreen = function () {
-  app.win.setFullScreen(!app.win.isFullScreen())
-}
+  win.setFullScreen(!win.isFullScreen());
+};
 
 app.toggleMenubar = function () {
-  app.win.setMenuBarVisibility(!app.win.isMenuBarVisible())
-}
+  win.setMenuBarVisibility(!win.isMenuBarVisible());
+};
 
 app.toggleVisible = function () {
-  if (process.platform !== 'darwin') {
-    if (!app.win.isMinimized()) { app.win.minimize() } else { app.win.restore() }
+  if (process.platform !== "darwin") {
+    if (!win.isMinimized()) {
+      win.minimize();
+    } else {
+      win.restore();
+    }
   } else {
-    if (isShown && !app.win.isFullScreen()) { app.win.hide() } else { app.win.show() }
+    if (isShown && !win.isFullScreen()) {
+      win.hide();
+    } else {
+      win.show();
+    }
   }
-}
+};
 
 app.injectMenu = function (menu) {
   try {
-    Menu.setApplicationMenu(Menu.buildFromTemplate(menu))
+    Menu.setApplicationMenu(Menu.buildFromTemplate(menu));
   } catch (err) {
-    console.warn('Cannot inject menu.')
+    console.warn("Cannot inject menu.");
   }
-}
+};
+*/
